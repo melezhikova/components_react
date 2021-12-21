3a6b3a4bb0a28914ebcb1bd465722b6176010170
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-12-21 14:25:40 +0500

    first commit

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import Item from './Item';
+
+export default function ShopItemFunc (props) {
+    console.log(props);
+    const { item } = props;
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{`${item.price}.00`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(Item).isRequired
+}
+  
